1587e25f4b3a35f8df4109466e8fc8843c401fca
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-06-18 16:03:49 +0200

    Connessione db

--- Testing/connectiondb.py
+++ Testing/connectiondb.py
@@ -4,12 +4,8 @@ uri = "mongodb+srv://Brunolcn:2ZfdeTz851ld7e6t@atlascluster.hz5otjy.mongodb.net/
 
 client = MongoClient(uri)
 
-db = client['sample_mflix']
-
-collection = db['movies']
-print(collection)
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
