4a2641970c9f495805efda2dfa2a9e28df585037
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-06-29 16:19:02 +0200

    Aggiunta connessione db

--- Testing/connectiondb.py
+++ Testing/connectiondb.py
@@ -1,11 +1,34 @@
 from pymongo import MongoClient
+from colorama import Fore, Style 
+import os
+#from utente import *
+        
+uri = "mongodb+srv://giorgiociampi:qODlA9DjoW2lpniv@cluster0.zfjgzzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-uri = "mongodb+srv://Brunolcn:2ZfdeTz851ld7e6t@atlascluster.hz5otjy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
+def connection_db():
+    global uri
+    try:
+        db = MongoClient(uri)
+        client = db['Test1']
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        return client
+    except Exception as e:
+        print(e)
+        
+def connection_collection(collection): # concerti e utenti
+    global uri
+    try:
+        client = connection_db()
+        collection = client[collection]
+        return collection
+    except Exception as e:
+        print(e)
 
-client = MongoClient(uri)
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+        
+def clear_screen():
+    if os.name == "nt":  
+        os.system("cls")
+    else:  
+        os.system("clear")
+        
+   
